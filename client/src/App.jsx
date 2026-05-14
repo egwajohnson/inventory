16a88b45123dc5aea0d96a00c2d55aff11e0b633
egwaj <egwajohnson.a@gmail.com>
2026-05-14 18:51:40 +0100

    feat: bootstrap install

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,4 @@
 import { RouterProvider } from "react-router-dom";
-// import Dashboard from "./module/dashboard/dashboard";
-// import Login from "./login";
-// import Unauthorized from "./Layout/components/unauthorized/unauthorized";
 import router from "./router";
 import "./js/bootstrap.bundle.min.js";
 import "./App.css";
@@ -9,6 +6,8 @@ import "./css/index.css";
 import "./css/bootstrap.css";
 import "./css/bootstrap.min.css";
 import "./css/bootstrap-grid.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return <RouterProvider router={router} />;
